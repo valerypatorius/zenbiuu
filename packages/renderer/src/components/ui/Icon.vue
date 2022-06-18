55cd321236566001aac3b62511a6039d81ae16ec
Valery <valerypatorius@yandex.ru>
2022-06-18 14:21:13 +0300

    Use Composition API in Icon.vue

--- packages/renderer/src/components/ui/Icon.vue
+++ packages/renderer/src/components/ui/Icon.vue
@@ -8,46 +8,33 @@
   />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script lang="ts" setup>
+import { computed } from 'vue';
 import icons, { IconString } from '@/assets/icons';
 
-export default defineComponent({
-  name: 'Icon',
-  props: {
-    /**
-     * Icon name
-     */
-    name: {
-      type: String,
-      required: true,
-    },
-  },
-  computed: {
-    /**
-     * Svg html string
-     */
-    svg (): IconString {
-      return icons[this.name];
-    },
-  },
-});
+const props = defineProps<{
+  /** Icon name */
+  name: string;
+}>();
+
+/** Svg html string */
+const svg = computed<IconString>(() => icons[props.name]);
 </script>
 
-<style>
+<style lang="postcss">
   .icon {
     flex-shrink: 0;
     display: block;
     width: 1.6rem;
     color: inherit;
     position: relative;
-  }
 
-  .icon svg {
-    fill: currentColor;
-    display: block;
-    width: 100%;
-    height: auto;
-    pointer-events: none;
+    svg {
+      fill: currentColor;
+      display: block;
+      width: 100%;
+      height: auto;
+      pointer-events: none;
+    }
   }
 </style>
